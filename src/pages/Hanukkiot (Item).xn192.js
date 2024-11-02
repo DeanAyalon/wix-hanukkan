2b01a4f473cb504165c10863f634b91f5663d0d6
Dean Ayalon <deanayalon@me.com>
2024-11-02 23:19:46 +0200

    Add multilingual support for hanukkiot (item) and out mission pages

--- src/pages/Hanukkiot (Item).xn192.js
+++ src/pages/Hanukkiot (Item).xn192.js
@@ -1,9 +1,21 @@
+import { multilingual } from "wix-window-frontend"
+
+// Handle LTR/RTL
+const rtl = multilingual.currentLanguage == 'he'
+if (rtl) {
+    $w('#main').collapse()
+    $w('#main-rtl').expand()
+}
+const model = rtl ? $w('#model-rtl') : $w('#model'),
+    showModel = rtl ? $w('#showModel-rtl') : $w('#showModel'),
+    img = rtl ? $w('#img-rtl') : $w('#img')
+
+// Load model from CMS
 $w('#dynamicDataset').onReady(() => {
     const url = $w('#dynamicDataset').getCurrentItem().widgetUrl
     if (url) {
-        $w('#model').src = url
-        $w('#loadModel').show()
-    } else $w('#model').hide()
+        model.src = url
+        showModel.show()
+    } else model.hide()
 })
-
-$w('#loadModel').onClick(() => $w('#img').hide())
+showModel.onClick(() => img.hide())

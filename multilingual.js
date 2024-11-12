250c8b62538f552b02e6e80af2e434f09928f141
Dean Ayalon <deanayalon@me.com>
2024-11-12 05:34:34 +0200

    Collapse multilingual elements before expansion, as language change does not reload page Now default collapse does not need to be set, but still better to set all multilinguals as collapsed

--- multilingual.js
+++ multilingual.js
@@ -3,8 +3,9 @@ import { multilingual } from 'wix-window-frontend'
 
 import _w from './selector'
 
-const rtl = multilingual.currentLanguage === 'he'
-let ext = rtl ? '-rtl' : '' // Tag extension for RTL elements
+export const rtl = multilingual.currentLanguage === 'he'    // Is the site in RTL mode
+const rtlExtension = '-rtl',                                // Tag extension for RTL elements
+    ext = rtl ? rtlExtension : ''                           // Active tag extension
 
 /** Multilingual selector, works like _w
  * adds '-rtl' to the element tag if site language is written in RTL */
@@ -14,8 +15,13 @@ export default {
     html: tag => _w.html(tag + ext),
 }
 
-/** Expand RTL and collapse LTR versions of the given elements according to current language
+/** Expand specified multilingual elements according to layout direction
  * @param {...string} elements Element tags (without '#' prefix) */
 export function expand(...elements) {
-    for (const element of elements) $w('#' + element + ext).expand()
+    for (const element of elements) {
+        // Collapse all multilingual elements, as the page does not reload when language is changed
+        _w.collapsable(`#${element}, #${element + rtlExtension}`).collapse()
+        // Expand elements according to layout direction
+        _w.collapsable('#' + element + ext).expand()
+    }
 }
